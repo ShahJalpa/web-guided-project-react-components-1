0e450053b6444953606b91f8f6169a0187626276
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-01-18 16:18:24 -0500

    web guidede project for understanding

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -8,3 +8,46 @@
   - We never tamper with state: `healthPoints++`, `healthPoints--` or `someState.push(item)` is FORBIDDEN
   - We use the dedicated "state updater" to schedule a state change: `setHealthPoints(healthPoints + 1)`
 */
+import React, { useState} from 'react';
+//useState allows to set internal state
+//it is a function that takes that desired initial state
+//returns an array with 2 things: the state itself and a state changer
+//NODECLARING VARIABLES TO HOLD STATE (with let)
+
+export default function Playground (props){
+  //return 'playground for ' + props.cohort;
+
+  // const theArray = useState(0);
+  // const count = theArray[0];
+  // const setCount = theArray[1];
+  const [count, setCount]= useState(0) //all above 3 lines code in one line
+  const [spinnerOn, setSpinnerON] = useState(true)
+  const [weapon, setWeapon] = useState('')
+  //let count = 0 NOOOOOOOOOO
+
+  if(spinnerOn){
+    return(
+      <div className="container" >
+        Loading...
+          <button onClick={event => {setSpinnerON(false)}}>turn spinner off</button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="container" id={'cohort' + props.cohort}>
+      <h3>Playground for {props.cohort}</h3>
+      <div>the count is {count}</div>
+        <button onClick = {event => { setCount(count + 1) }}>increase</button>
+
+      <div>the spinner is  {spinnerOn ? 'ON' : 'OFF'}</div>
+        <button onClick = {event => setSpinnerON(!spinnerOn)}>toggle spinner</button>
+
+      <div>the current weapon is {weapon}</div>
+        <button onClick = {event => {setWeapon ('scissor')}}>pick scissor</button>
+        <button onClick = {event => {setWeapon ('rock')}}>pick rock</button>
+        <button onClick = {event => {setWeapon ('paper')}}>pick paper</button>
+      
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Playground from './components/Playground' // importing playground.js as import react-dom happens only once per app.
 
 /*
 💥💥💥 Rules when DECLARING a React component 💥💥💥
@@ -21,15 +22,37 @@ import { render } from 'react-dom'
   - Don't forget all tags need to close correctly
 */
 
-function App(props) {
+//We can have many functions (components) // with React components (function) name should start with capital letter.
+/*function App(props) { // one object called 'props' // /* props can make components versitle
   return (
     <div className='container'>
-      <h1>Welcome to React, Web {props.cohort}</h1>
+      <h1>Welcome to React, Web {props.cohort}</h1> 
+      <div>{props.instructor} welcomes you!</div>
+    </div>
+  )
+}*/
+
+//Another way to write
+function App(props) { // one object called 'props' // /* props can make components versitle // happy takes boolean value
+  const { cohort, instructor, happy, week} = props
+  return (
+    <div className='container'>
+      <h1>Welcome to React, Web {cohort}</h1> 
+      <div>{instructor} welcomes you!</div>
+
+      {happy ? <div>Vary happy</div> : <div>It is Monday</div>}
+
+      <div> It is a week {week}</div>
+
+      <Playground cohort = {cohort} />
     </div>
   )
 }
 
+
+//it can be used only once per app usally
 render(
-  <App cohort='37' />,
+  //<App cohort='39' instructor = 'Rhiannon' />, // element(s)
+  <App cohort='39' instructor = 'Rhiannon' happy={false} week = {2} />, //code with boolean value
   document.querySelector('#root')
 )
